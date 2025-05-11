a53205c847f829579e93ba8152355fc0d7a95fd9
lewan87 <nawemarchelli@gmail.com>
2025-05-11 00:47:01 -0300

    added tilemap

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -12,8 +12,12 @@ func _ready():
 	pass
 	
 func _process(delta: float):
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up","down")
+	).normalized()
 	pass
 
 func _physics_process(delta: float):
